7b4f3675b7597f66030f2ed23da021743d0c5bb9
Higinio Santos <101358454+Higinio01@users.noreply.github.com>
2025-06-02 15:25:27 -0400

    test(red)

--- src/test/java/ServicioNominaTest.java
+++ src/test/java/ServicioNominaTest.java
@@ -11,7 +11,7 @@ public class ServicioNominaTest {
     public void calcularSalario_SinHorasExtra_DeberiaRetornarPagoBase() {
         Empleado empleado = new Empleado("Ana", TipoEmpleado.FULL_TIME, 200.0, true);
         ServicioNomina servicio = new ServicioNomina();
-        double salario = servicio.calcularSalarioSemanal(empleado, 38);
+        double salario = servicio.calcularSalario(empleado, 38);
 
         assertEquals(7600.0, salario);  // 38 * 200
     }
@@ -21,7 +21,7 @@ public class ServicioNominaTest {
         Empleado empleado = new Empleado("Luis", TipoEmpleado.FULL_TIME, 200.0, true);
         ServicioNomina servicio = new ServicioNomina();
         // 40h normales + 5h extra con 1.5x: (40 * 200) + (5 * 200 * 1.5) = 8000 + 1500 = 9500
-        double salario = servicio.calcularSalarioSemanal(empleado, 45);
+        double salario = servicio.calcularSalario(empleado, 45);
 
         assertEquals(9500.0, salario);
     }
@@ -31,7 +31,7 @@ public class ServicioNominaTest {
         Empleado empleado = new Empleado("Mario", TipoEmpleado.PART_TIME, 200.0, true);
         ServicioNomina servicio = new ServicioNomina();
         // No se pagan horas extra: 45 * 200 = 9000
-        double salario = servicio.calcularSalarioSemanal(empleado, 45);
+        double salario = servicio.calcularSalario(empleado, 45);
 
         assertEquals(9000.0, salario);
     }
